3f6e2723b77d78c5fab2e31ee723e65285a33b38
Ashley Amato <ashleyamato1@gmail.com>
2017-10-11 16:00:38 -0600

    finish checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-// Your JS goes here
+// Hints
+//
+// Each tile should be a div
+// Each tile's width is 11.1%
+// Set each tile's float property to left
+// Each tile's paddingBottom is 11.1%
+// function that runs several times
+for (let i = 0; i < 4; i++) {
+  for (let i = 0; i < 9; i++) {
+    var div = document.createElement('div')
+    var body = document.getElementsByTagName('body')[0]
+    body.appendChild(div)
+    div.style.width = '11.1%'
+    div.style.float = 'left'
+    div.style.paddingBottom = '11.1%'
+      if (i % 2 === 0) {
+        div.style.backgroundColor = 'black'
+      } else {
+        div.style.backgroundColor = 'red'
+      }
+  }
+  for (let i = 0; i < 9; i++) {
+    var div = document.createElement('div')
+    var body = document.getElementsByTagName('body')[0]
+    body.appendChild(div)
+    div.style.width = '11.1%'
+    div.style.float = 'left'
+    div.style.paddingBottom = '11.1%'
+      if (i % 2 === 0) {
+        div.style.backgroundColor = 'red'
+      } else {
+        div.style.backgroundColor = 'black'
+      }
+  }
+}
