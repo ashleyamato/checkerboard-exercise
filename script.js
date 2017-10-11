83599a8b90ab6d7423a3147f489aa34e6417a234
Ashley Amato <ashleyamato1@gmail.com>
2017-10-11 16:16:24 -0600

    add random colors

--- script.js
+++ script.js
@@ -6,6 +6,14 @@
 // Each tile's paddingBottom is 11.1%
 // function that runs several times
 for (let i = 0; i < 4; i++) {
+  function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
   for (let i = 0; i < 9; i++) {
     var div = document.createElement('div')
     var body = document.getElementsByTagName('body')[0]
@@ -13,11 +21,8 @@ for (let i = 0; i < 4; i++) {
     div.style.width = '11.1%'
     div.style.float = 'left'
     div.style.paddingBottom = '11.1%'
-      if (i % 2 === 0) {
-        div.style.backgroundColor = 'black'
-      } else {
-        div.style.backgroundColor = 'red'
-      }
+    div.style.backgroundColor = getRandomColor()
+
   }
   for (let i = 0; i < 9; i++) {
     var div = document.createElement('div')
@@ -26,10 +31,6 @@ for (let i = 0; i < 4; i++) {
     div.style.width = '11.1%'
     div.style.float = 'left'
     div.style.paddingBottom = '11.1%'
-      if (i % 2 === 0) {
-        div.style.backgroundColor = 'red'
-      } else {
-        div.style.backgroundColor = 'black'
-      }
+    div.style.backgroundColor = getRandomColor()
   }
 }
