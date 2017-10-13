685a24d562eb4333ec1ac9d7452f842d7b68cf0d
Ashley Amato <ashleyamato1@gmail.com>
2017-10-12 21:49:48 -0600

    finish gradient

--- script.js
+++ script.js
@@ -6,24 +6,20 @@
 // Each tile's paddingBottom is 11.1%
 // function that runs several times
 for (let i = 0; i < 4; i++) {
-  function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
+  for (let i = 0; i < 9; i++) {
+    var div = document.createElement('div')
+    var body = document.getElementsByTagName('body')[0]
+    body.appendChild(div)
+    div.style.width = '11.1%'
+    div.style.float = 'left'
+    div.style.paddingBottom = '11.1%'
+    if (i % 2 === 0) {
+    div.style.background = 'purple'
+  } else {
+    div.style.background = 'pink'
   }
-  return color;
 }
-  for (let i = 0; i < 9; i++) {
-    var div = document.createElement('div')
-    var body = document.getElementsByTagName('body')[0]
-    body.appendChild(div)
-    div.style.width = '11.1%'
-    div.style.float = 'left'
-    div.style.paddingBottom = '11.1%'
-    div.style.backgroundColor = getRandomColor()
 
-  }
   for (let i = 0; i < 9; i++) {
     var div = document.createElement('div')
     var body = document.getElementsByTagName('body')[0]
@@ -31,6 +27,13 @@ for (let i = 0; i < 4; i++) {
     div.style.width = '11.1%'
     div.style.float = 'left'
     div.style.paddingBottom = '11.1%'
-    div.style.backgroundColor = getRandomColor()
+    div.style.zIndex= '1'
+    if (i % 2 === 0) {
+    div.style.background = 'pink'
+    } else {
+    div.style.background = 'purple'
+    }
   }
+  body.style.background = "linear-gradient(to bottom,black,teal,red)"
+  body.style.opacity = '.4'
 }
